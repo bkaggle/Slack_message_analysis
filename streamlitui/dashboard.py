406899f3f7e15c7019261f50dcfb11d579caabd9
Naol Lamesa <malkaamedia@gmail.com>
2023-12-01 21:57:22 +0300

    dashboard

--- streamlitui/dashboard.py
+++ streamlitui/dashboard.py
@@ -2,45 +2,68 @@ import streamlit as st
 import os, sys
 import json
 import glob
+import re
+from gensim import corpora
 import pandas as pd
+import pickle
 from matplotlib import pyplot as plt
 import seaborn as sns
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
 
 
+with open('notebooks/lda_model.pkl', 'rb') as f:
+    lda_model = pickle.load(f)
 df = pd.read_csv('slack_data.csv')
+reactions_df = pd.read_csv('data/reactions.csv')
+mentions_df = pd.read_csv('data/mentions.csv')
+
+df['msg_content'] = df['msg_content'] .astype(str) 
+# Casting to type str 
+df['msg_content']  = df['msg_content'] .apply(lambda x: x.lower())
+# Converting rows under the column 'full_text' to lowercase
+df['msg_content']  = df['msg_content'] .apply(lambda x: x.translate(str.maketrans(' ', ' ',' ')))
+
+sentence_list = [message for message in df['msg_content']]
+word_list = [sent.split() for sent in sentence_list]
+
+id2word  = corpora.Dictionary(word_list)
+corpus= [id2word.doc2bow(message) for message in word_list]
+
 # Display the DataFrame
 st.title("10 Academy")
-st.dataframe(df)
+# Function to generate word cloud from LDA model
+def generate_wordcloud_from_text(text_list):
+    text = ' '.join(text_list)
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    return wordcloud
 
-# Sidebar for filtering options
-st.sidebar.title("Filter Options")
 
-# Filtering by channel
-selected_channel = st.sidebar.selectbox("Select Channel", df['channel'].unique())
+# Display the top users
+Top_10_users_by_replies = df.groupby('sender_name')['reply_users_count'].sum().reset_index(name='Top_10_users_by_replies').sort_values(by='Top_10_users_by_replies', ascending=False).head(10)
+Top_10_users_by_messages = df.groupby('sender_name').size().reset_index(name='Top_10_users_by_messages').sort_values(by='Top_10_users_by_messages', ascending=False).head(10)
+Top_10_users_by_reactions = df.groupby('sender_name').size().reset_index(name='Top_10_users_by_reactions').sort_values(by='Top_10_users_by_reactions', ascending=False).head(10)
+Top_10_users_by_mentions = mentions_df.groupby(['sender_name']).size().reset_index(name='Top_10_users_by_mentions').sort_values(by='Top_10_users_by_mentions', ascending=False).head(10)
 
-# Filtering by message type
-selected_msg_type = st.sidebar.selectbox("Select Message Type", df['msg_type'].unique())
+st.header('Word Cloud from Streamlit')
+wordcloud_text = ' '.join(df['msg_content'].tolist())
+wordcloud = generate_wordcloud_from_text([wordcloud_text])
+st.image(wordcloud.to_array(), width=800)
 
-# Filter the DataFrame based on user selection
-filtered_df = df[(df['channel'] == selected_channel) & (df['msg_type'] == selected_msg_type)]
+# Top Users Section
+st.header('Top 10 Users by Replies')
+st.table(Top_10_users_by_replies)
 
-# Display filtered DataFrame
-st.subheader("Filtered DataFrame")
-st.dataframe(filtered_df)
+st.header('Top 10 Users by Messages')
+st.table(Top_10_users_by_messages)
 
-# Visualize top 10 users by number of messages
-st.subheader("Top 10 Users by Number of Messages")
-top_messages_per_user = filtered_df['sender_name'].value_counts().nlargest(10)
-st.bar_chart(top_messages_per_user)
+st.header('Top 10 Users by Reactions')
+st.table(Top_10_users_by_reactions)
 
-# Visualize top 10 users by number of replies
-st.subheader("Top 10 Users by Number of Replies")
-top_replies_per_user = filtered_df.groupby('sender_name').size().nlargest(10)
-st.bar_chart(top_replies_per_user)
+st.header('Top 10 Users by Mentions')
+st.table(Top_10_users_by_mentions)
 
-# Visualize top 10 users by number of reactions
-st.subheader("Top 10 Users by Number of Reactions")
-top_reactions_per_user = filtered_df.groupby('sender_name')['reply_users_count'].size().nlargest(10)
-st.bar_chart(top_reactions_per_user)
+# Optional: Display the LDA model topics
+st.header('LDA Model Topics')
+for topic_num, words in lda_model.print_topics():
+    st.write(f'Topic {topic_num}: {words}')
